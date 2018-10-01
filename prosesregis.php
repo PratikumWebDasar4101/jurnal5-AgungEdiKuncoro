bc5cde1c658f034cfff31db49b39f3326e708c51
AgungEdiKuncoro <42725305+AgungEdiKuncoro@users.noreply.github.com>
2018-10-01 10:42:36 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,7 +1,16 @@
-<?php
-
-
-
-
-
-?>
+<table border="1">
+	<th>Nama</th>
+	<th>NIM</th>
+	<th>Email</th>
+	<tr>
+		<td>
+			<?php echo $_POST['nama'];?>
+		</td>
+		<td>
+			<?php echo $_POST['email'];?>
+		</td>
+		<td>
+			<?php echo $_POST['nim'];?>
+		</td>
+	</tr>
+</table>
