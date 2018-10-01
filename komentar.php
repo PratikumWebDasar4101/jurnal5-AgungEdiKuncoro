9ebfc682832d52c257ed29c3618c621ad00efb03
AgungEdiKuncoro <42725305+AgungEdiKuncoro@users.noreply.github.com>
2018-10-01 10:07:42 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,31 @@
+<form method="post">
+<table align="center">
+	<tr align="center">
+		<td>
+			Kolom Komentar
+		</td>
+	</tr>
+	<tr>
+		<td width="500px">
+			<textarea cols="70" rows="10" placeholder="Masukan komentar minimal 5 karakter." name="komentar"></textarea>
+		</td>
+	</tr>
+	<tr>
+		<td align="right">	
+			<input type="submit" name="submit">
+		</td>
+	</tr>
+</table>
+</form>
 <?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
+ if (isset($_POST['komentar'])) {
+ 	$komentar = $_POST['komentar'];
+ 	$hitung	= strlen($komentar);
 
-
-
-
+ 	if ($hitung < 5) {
+ 		echo "Masukan komentar sesuai dengan karakter minimal! (5)";
+ 	}else{
+ 		echo "Jumlah karakter komentar anda : ".$hitung;
+ 	}
+ }
 ?>
